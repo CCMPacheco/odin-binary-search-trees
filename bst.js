d8dcc6abd26a0efc7d99f4361447f4bedc976b23
CCMPacheco <pachecoccmp@gmail.com>
2023-01-28 20:37:19 -0300

    buildTree function completed

---
+++ bst.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.buildTree(sortedArray);
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    const mid = Math.floor(array.length / 2);
+    const newNode = new Node(array[mid]);
+    newNode.left = this.buildTree(array.slice(0, mid));
+    newNode.right = this.buildTree(array.slice(mid + 1));
+
+    return newNode;
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+prettyPrint(tree.root);
